42a87428245f19603b491402c5a9848977aafcad
Jaan Toots <jaantoots@gmail.com>
2016-07-21 00:00:49 +0100

    Outline structure

--- main.lua
+++ main.lua
@@ -6,5 +6,25 @@ require 'cudnn'
 
 -- Load the datasets
 -- TODO: Return random permutations
-trainset = torch.load('train.t7'):cuda()
-testset = torch.load('test.t7'):cuda()
+local trainset = torch.load('train.t7')
+local testset = torch.load('test.t7')
+
+-- Set metatable to return [i]th sample and func for size
+
+-- Normalize data
+
+-- Define model
+--[[
+   Use VGG configuration D or E: conv3 layers with ReLU and maxpooling
+   No FC layers, image output
+   Softmax loss
+   Use dropout and batch normalization
+--]]
+
+-- Define loss function
+
+-- Train the network
+
+-- Validate
+
+-- Test
