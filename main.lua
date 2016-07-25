e677e39af5650804e34c9320038a7dd195801732
Jaan Toots <jaantoots@gmail.com>
2016-07-25 20:17:45 +0100

    Implement training

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
-local torch = require "torch"
+require "torch"
 require "cutorch"
-local nn = require "nn"
+require "nn"
 local cudnn = require "cudnn"
+local optim = require "optim"
 
 -- Dataset handling methods
 -- TODO: Return random permutations
 local data = require "data"
+-- TODO: Calculate weights
+local weights = {}
 
 -- Set metatable to return [i]th sample and func for size
 
@@ -13,10 +16,38 @@ local data = require "data"
 
 -- Network and loss function
 local net = require "model"
-local criterion = cudnn.SpatialCrossEntropyCriterion()
+local criterion = cudnn.SpatialCrossEntropyCriterion(weights)
 criterion = criterion:cuda()
 
 -- Train the network
+net:training()
+local maxIterations = 1000
+local batchSize = 8
+local config = {
+  learningRate = 1e-1,
+  alpha = 0.99,
+  epsilon = 1e-6
+}
+local params, gradParams = net:getParameters()
+for i = 1, maxIterations do
+  -- Get the minibatch
+  local batch = data.batch(batchSize)
+  local batchInputs = batch.inputs
+  local batchLabels = batch.labels
+  -- TODO: Check weights initialization
+
+  local function feval (_)
+    -- For optim, outputs f(X): loss and df/dx: gradients
+    gradParams:zero()
+    local outputs = net:forward(batchInputs)
+    local loss = criterion:forward(outputs, batchLabels)
+    local gradLoss = criterion:backward(outputs, batchLabels)
+    net:backward(batchInputs, gradLoss)
+    print(i, loss)
+    return loss, gradParams
+  end
+  optim.rmsprop(feval, params, config)
+end
 
 -- Validate
 
