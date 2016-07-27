6f6f86bc27123dc58c5c8b2b2b9f50c99cdc90ed
Jaan Toots <jaantoots@gmail.com>
2016-07-27 18:07:10 +0100

    Document helpers.dice

--- helpers.lua
+++ helpers.lua
@@ -2,8 +2,23 @@ local torch = require "torch"
 
 local helpers = {}
 
+
+
 function helpers.dice (outputs, targets)
-  -- Calculate accuracy score as Dice coefficient
+  --[[ Calculate accuracy score as Dice coefficient
+
+  Parameters
+  ----------
+  outputs: torch.Tensor of size [batchSize] x 2 x height x width, network output
+      containing class scores for each pixel
+  targets: torch.Tensor of size [batchSize] x height x width, ground truth
+      labels containing class indices for each pixel
+
+  Returns
+  -------
+  coeffs: torch.Tensor of size [batchSize] (or double) containing the Dice
+      scores for each image in the batch
+  --]]
   local _, predictions = outputs:max(outputs:dim() - 2)
   predictions = predictions:squeeze():double() - 1
   targets = targets:double() - 1
@@ -16,9 +31,9 @@ function helpers.dice (outputs, targets)
   dens = dens:sum(dens:dim()):squeeze()
   dens = dens:sum(dens:dim())
   -- Coefficient
-  local coeff = 2*torch.cdiv(nums, dens):squeeze()
-  coeff[coeff:ne(coeff)] = 1 -- by definition if both sets are zero
-  return coeff
+  local coeffs = 2*torch.cdiv(nums, dens):squeeze()
+  coeffs[coeffs:ne(coeffs)] = 1 -- by definition if both sets are zero
+  return coeffs
 end
 
 return helpers
